6d9c91284a491ec049b5fd44fc4d7dc95c565def
Vaibhav Dhunde <vaibhavdhunde94@gmail.com>
2020-01-17 20:47:29 +0530

    Establish database connection

---
+++ include/DbConfig.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * DB CONSTANTS
+ */
+define('DB_NAME', 'visitors');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_HOST', 'localhost');
+
+?>
--- v1/index.php
+++ v1/index.php
@@ -1,6 +1,7 @@
 <?php
 
 require '../libs/vendor/autoload.php';
+require_once '../include/DbConnect.php';
 
 $app = new Slim\App();
 
@@ -8,6 +9,15 @@ $app->get('/hello/{name}', function ($request, $response, $args) {
     $name = $args['name'];
     $message = "Hello, " . $name . "!";
     return $response->write($message);
+});
+
+$app->post('/conncheck', function ($request, $response, $args) {
+    $db = new DbConnect();
+    $conn = $db->connect();
+    if ($conn != null) {
+        $message = "Database connection established successfully!";
+        return $response->write($message);
+    }
 });
 
 $app->run();
